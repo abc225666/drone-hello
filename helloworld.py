f96de7fd8d3356cbcdb56d517cd5deb8694174b2
Ting-Rui, Wang <qwe50120@gmail.com>
2018-10-09 18:54:03 +0000

    test

--- helloworld.py
+++ helloworld.py
@@ -29,7 +29,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("zzHello, world!")
+        self.write("test pr")
 
 
 def main():
